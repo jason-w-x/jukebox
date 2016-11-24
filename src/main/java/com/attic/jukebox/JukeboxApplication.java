cf157a9e98d5ecec334986798f539ac12408a5c1
Jason Xie <jasonx@umich.edu>
2016-11-24 00:17:30 -0500

    guice setup

--- src/main/java/com/attic/jukebox/JukeboxApplication.java
+++ src/main/java/com/attic/jukebox/JukeboxApplication.java
@@ -1,10 +1,17 @@
 package com.attic.jukebox;
 
 import com.attic.jukebox.resources.JukeboxResource;
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class JukeboxApplication extends Application<JukeboxAppConfiguration> {
+    @Override
+    public void initialize(final Bootstrap<JukeboxAppConfiguration> bootstrap) {
+        bootstrap.addBundle(GuiceBundle.<JukeboxAppConfiguration>newBuilder().addModule(new JukeboxModule()).setConfigClass(JukeboxAppConfiguration.class).build());
+    }
+
     @Override
     public void run(final JukeboxAppConfiguration configuration, final Environment environment) throws Exception {
         JukeboxResource resource = new JukeboxResource(configuration.getTemplate(), configuration.getDefaultName());
---
+++ src/main/java/com/attic/jukebox/JukeboxModule.java
@@ -0,0 +1,10 @@
+package com.attic.jukebox;
+
+import com.google.inject.AbstractModule;
+
+public class JukeboxModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
